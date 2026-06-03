0c8c1c0541226c00acaf5575f19b1c1c6911ad23
shabrinanifa <sabrina.rina1414@gmail.com>
2026-06-03 22:30:25 +0700

    progress-8-CRUD

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,6 +13,16 @@ $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
 
 $routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+
+$routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+});
+
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 
-$routes->get('profile', 'Profile::index', ['filter' => 'auth']);
+$routes->get('faq', 'Home::faq', ['filter' => 'auth']);
+$routes->get('profile', 'Home::profile', ['filter' => 'auth']);
+$routes->get('contact', 'Home::contact', ['filter' => 'auth']);
