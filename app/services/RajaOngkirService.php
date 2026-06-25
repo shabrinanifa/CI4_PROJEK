bd59d0d57f3d46fc002de0f865e289e8f5d859eb
shabrinanifa <sabrina.rina1414@gmail.com>
2026-06-25 13:46:14 +0700

    progress-11.1-webservice-server

--- app/services/RajaOngkirService.php
+++ app/services/RajaOngkirService.php
@@ -13,6 +13,7 @@ class RajaOngkirService
         $this->client = Services::curlrequest([
             'timeout' => 10,
             'http_errors' => false,
+            'verify' => false,
         ]);
 
         $this->apiKey = env('RAJAONGKIR_API_KEY');
